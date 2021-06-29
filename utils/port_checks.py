d3d25f4924ab671a9544013a46bd3b79d47c9fa7
Piotr Iwaniuk <piwaniuk@esri.pl>
2021-06-29 14:12:58 +0200

    port checks renamed

--- utils/port_checks.py
+++ utils/port_checks.py
@@ -8,10 +8,11 @@ warnings.filterwarnings("ignore")
 
 def is_port_open(host, port):
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-        return sock.connect_ex((host, port)) == 0
+        sock.settimeout(1)
+        return sock.connect_ex((host.strip(), port)) == 0
 
 
-def is_valid(url, method='post'):
+def is_url_valid(url, method='get'):
     response = getattr(r, method)(url, verify=False)
     return response.ok
 
@@ -19,7 +20,6 @@ def is_valid(url, method='post'):
 if __name__ == "__main__":
     host = r"wp.pl"
     port = 443
-    url = 'https://{}:{}'.format(host, port)
 
     print(is_port_open(host, port))
-    print(is_valid(url))
+    print(is_url_valid('https://{}:{}'.format(host, port)))
