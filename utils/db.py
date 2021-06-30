d1a1822f753846b49dd3cda3fc363e6f07bb692e
Piotr Iwaniuk <piwaniuk@esri.pl>
2021-06-30 13:38:14 +0200

    db connectors

---
+++ utils/db.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import pyodbc
+import numpy as np
+
+
+class SQL_CONNECTOR:
+    def __init__(self, connection_string):
+        self.connection = pyodbc.connect(connection_string)
+        self.cursor = self.connection.cursor()
+
+    def select(self, table, fields="*", clauses=""):
+        if isinstance(fields, list):
+            fields = ','.join(fields)
+        sql = f"select {fields} from {table} {clauses}"
+        df = pd.read_sql(sql, self.connection)
+        return df
+
+    def exec(self, statement):
+        result = self.cursor.execute(statement)
+        self.cursor.commit()
+        return result
+
+    def insert(self, table, fields, rows):
+        fields_len = len(fields)
+        if not all(len(row) == fields_len for row in rows):
+            raise Exception("row lengts different than fields count")
+
+        values_placeholder = ','.join(fields_len * "?")
+        rows_unnumpyed = [
+            [int(v) if type(v) == np.int64 else v for v in row]
+            for row in rows
+        ]
+
+        sql = f"insert into {table} ({','.join(fields)}) values ({values_placeholder})"
+        self.cursor.executemany(sql, rows_unnumpyed)
+        self.cursor.commit()
+
+
+class SDE_CONNECTOR(SQL_CONNECTOR):
+    def get_new_object_id(self, table):
+        sql = """
+            DECLARE @out int;
+            EXEC sde.next_rowid ?, ?, @rowid = @out OUTPUT;
+            SELECT @out AS objectid;
+        """
+
+        *_, user, table_name = table.split('.')
+        result = self.cursor.execute(sql, user, table_name)
+        return result.fetchone()[0]
